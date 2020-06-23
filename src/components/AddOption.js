ba68fc1a6992447c2619c2e5e02c21ffde670bef
wongprom <jimmybackstrom@hotmail.com>
2020-06-23 14:19:23 +0200

    70, styling option item, and form.

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -25,9 +25,11 @@ class AddOption extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.addOptionFormHandler}>
-          <input type="text" name="option" />
+        {this.state.error && (
+          <p className="add-option-error">{this.state.error}</p>
+        )}
+        <form className="add-option" onSubmit={this.addOptionFormHandler}>
+          <input className="add-option__input" type="text" name="option" />
           <button className="button">Add Option</button>
         </form>
       </div>
