83c3909e36d0a671d76df70c1b1f7ff8dde172e7
wongprom <jimmybackstrom@hotmail.com>
2020-06-23 15:08:32 +0200

    71, styling react-modal. Styling thirdparty packages.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,13 +3,19 @@ import Modal from 'react-modal';
 
 const OptionModal = (props) => (
   <Modal
+    closeTimeoutMS={200}
     isOpen={!!props.selectedOption}
     onRequestClose={props.onCloseModal}
     contentLabel="selected option"
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.onCloseModal}>Close</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && (
+      <p className="modal__body">{props.selectedOption}</p>
+    )}
+    <button className="button" onClick={props.onCloseModal}>
+      Close
+    </button>
   </Modal>
 );
 
