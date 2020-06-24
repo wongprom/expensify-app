0913751e9f9f0b39499375c6a99ee093fa06c9fd
wongprom <jimmybackstrom@hotmail.com>
2020-06-24 10:38:34 +0200

    77,Reeact-Router 101. Setting up webpack.config with (historyApiFallback: true) to serve index.html. Created Route (pages) for render.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => <div>This is from ExpenseDasshboard</div>;
+const AddExpensePage = () => <div>This is from AddEcpensePage</div>;
+const EditExpensePage = () => <div>This is from EditExpensePage</div>;
+const HelpPage = () => <div>This is from HelpPage</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={ExpenseDashboardPage} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route path="/edit" component={EditExpensePage} />
+    </div>
+  </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById('app'));
