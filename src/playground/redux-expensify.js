6f19193d7115a55d1d82725721f642b14e8432fc
wongprom <jimmybackstrom@hotmail.com>
2020-06-27 17:43:02 +0200

    Playground 95. Dispatched the last setStartDate and setEndDate actions.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -38,9 +38,26 @@ const setTextFilter = (text = '') => ({
 });
 
 // SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE',
+});
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT',
+});
+
 // SET_START_DATE
+const setStartDate = (startDate) => ({
+  type: 'SET_START_DATE',
+  startDate,
+});
+
 // SET_END_DATE
+const setEndDate = (endDate) => ({
+  type: 'SET_END_DATE',
+  endDate,
+});
 
 //* Expenses Reducer
 const expensesReduserDefaultState = [];
@@ -81,6 +98,23 @@ const filtersReducer = (state = filterReducerDefaultState, action) => {
         ...state,
         text: action.text,
       };
+    case 'SORT_BY_AMOUNT':
+      return { ...state, sortBy: 'amount' };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date',
+      };
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.startDate,
+      };
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.endDate,
+      };
     default:
       return state;
   }
@@ -113,11 +147,18 @@ const expenseTwo = store.dispatch(
 );
 // console.log('expenseOne', expenseOne);
 // console.log('expenseTwo', expenseTwo);
-store.dispatch(removeExpense({ id: expenseOne.expenses.id }));
-store.dispatch(editExpense(expenseTwo.expenses.id, { amount: 1234456 }));
+// store.dispatch(removeExpense({ id: expenseOne.expenses.id }));
+// store.dispatch(editExpense(expenseTwo.expenses.id, { amount: 1234456 }));
 
-store.dispatch(setTextFilter('Rent'));
-store.dispatch(setTextFilter());
+// store.dispatch(setTextFilter('Rent'));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(1250));
 
 //!DEMO CODE
 const demoState = {
