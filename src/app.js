02a59fd6e5b277f4740f8591f2680676526001bb
wongprom <jimmybackstrom@hotmail.com>
2020-06-29 12:57:35 +0200

    99, organizing redux. Moved redux state from playground to src/app.js.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectores/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+store.subscribe(() => {
+  const state = store.getState();
+  console.log(state);
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log('visibleExpenses', visibleExpenses);
+});
+
+// addExpenses -> Water bill
+store.dispatch(
+  addExpense({
+    description: 'Water bill',
+  })
+);
+// addExpenses -> Gas bill
+store.dispatch(
+  addExpense({
+    description: 'Gas bill',
+    amount: 1234,
+    note: 'gas bill for our "summer house"',
+  })
+);
+// setTextFilters -> bill (2 item) -> water (1 item)
+store.dispatch(setTextFilter('wa'));
+// getVisibleExpenses -> print visibles ones on screeen
+// console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
