3227e1722ee1fd3a39e8d19a45678eedf106f56c
wongprom <jimmybackstrom@hotmail.com>
2020-07-01 16:09:02 +0200

    107, Wiring up Add Expense. dispatch a new expense from parent component.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -13,6 +13,7 @@ class ExpenseForm extends React.Component {
     amount: '',
     createdAt: moment(),
     calenderFocused: false,
+    error: '',
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -24,21 +25,44 @@ class ExpenseForm extends React.Component {
   };
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   };
   onDateChange = (createdAt) => {
-    this.setState(() => ({ createdAt }));
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
   };
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calenderFocused: focused }));
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({
+        error: 'Please provide description and amount.',
+      }));
+      // set error state equal to " Please provide description and amount."
+    } else {
+      this.setState(() => ({ error: '' }));
+      // Clear the error
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note,
+      });
+      console.log('form submitted');
+    }
   };
 
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
