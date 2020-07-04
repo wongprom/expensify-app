819a81aeb4129a76d4f80e3a9ba2389f1ea9d0f9
wongprom <jimmybackstrom@hotmail.com>
2020-07-04 16:47:48 +0200

    119. Enzyme.

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,14 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+
 import React from 'react';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
-  console.log(renderer.getRenderOutput());
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
