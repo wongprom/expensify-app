6ddb1a8edb85cf6fb943c3b112bdcb4173f90102
wongprom <jimmybackstrom@hotmail.com>
2020-07-08 11:23:41 +0200

    Removed dummy data.

--- src/app.js
+++ src/app.js
@@ -15,34 +15,6 @@ import uuid from 'uuid';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-});
-
-//* Add Expense
-// addExpenses -> Water bill
-store.dispatch(
-  addExpense({
-    description: 'Water bill',
-    amount: 4500,
-  })
-);
-// addExpenses -> Gas bill
-store.dispatch(
-  addExpense({
-    description: 'Gas bill',
-    createdAt: 1000,
-  })
-);
-// addExpenses -> Rent
-store.dispatch(
-  addExpense({
-    description: 'Rent',
-    amount: 109500,
-  })
-);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
